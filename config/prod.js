062df60da1eb56df313a63c44d21642a4293e09a
toanphan0303 <nein0303@gmail.com>
2018-03-29 18:35:24 -0700

    heroku push 1

---
+++ config/prod.js
@@ -0,0 +1,11 @@
+module.exports ={
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  fbClientID: process.env.FACEBOOK_CLIENT_ID,
+  fbClientSecrete: process.env.FACEBOOK_CLIENT_SECRET,
+  cookieSecret: process.env.COOKIE_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  awsAccessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  awsSecretAccessKey: process.env.AWS_SECRET_ACCESS_KEY ,
+  S3Bucket: process.env.S3_BUCKET
+}
