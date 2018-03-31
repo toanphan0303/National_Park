0b4a5bb215dc66676efee42b8801fb4cccf2e03a
toanphan0303 <nein0303@gmail.com>
2018-03-30 20:44:36 -0700

    fix leak atempt

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const https = require('https')
 const pem = require('pem')
 const PORT = process.env.PORT || 5000;
-//
-// pem.createCertificate({ days: 1, selfSigned: true}, (err, keys) =>{
-//   if(err){
-//     throw err
-//   }
-//   const app = require('./server/server');
-//   console.log('app listening on port', PORT)
-//   https.createServer({ key: keys.serviceKey, cert: keys.certificate}, app).listen(PORT)
-// })
-const app = require('./server/server');
-app.listen(PORT || 5000, () =>{
+
+pem.createCertificate({ days: 1, selfSigned: true}, (err, keys) =>{
+  if(err){
+    throw err
+  }
+  const app = require('./server/server');
   console.log('app listening on port', PORT)
+  https.createServer({ key: keys.serviceKey, cert: keys.certificate}, app).listen(PORT)
 })
+// const app = require('./server/server');
+// app.listen(PORT || 5000, () =>{
+//   console.log('app listening on port', PORT)
+// })
